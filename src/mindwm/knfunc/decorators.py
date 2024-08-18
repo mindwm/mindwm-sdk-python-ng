0d7f0ac27a9d33ecb9341181b95f96c4f75a0da0
Sergey Bubnov <omgbebebe@gmail.com>
2024-08-19 01:04:33 +0400

    implement @with_trace decorator for knfunc decorators

--- src/mindwm/knfunc/decorators.py
+++ src/mindwm/knfunc/decorators.py
@@ -2,6 +2,7 @@ import inspect
 import json
 import os
 from base64 import b64decode
+from collections.abc import Callable
 from functools import wraps
 from uuid import uuid4
 
@@ -14,9 +15,53 @@ from mindwm import logging
 from mindwm.model.events import (CloudEvent, IoDocumentEvent, LLMAnswerEvent,
                                  TouchEvent)
 from neontology import auto_constrain, init_neontology
+from opentelemetry import trace
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import \
+    OTLPLogExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import \
+    OTLPSpanExporter as OTLPSpanGrpcExporter
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk.resources import HOST_NAME, SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (BatchSpanProcessor,
+                                            ConsoleSpanExporter)
+from opentelemetry.trace.propagation.tracecontext import \
+    TraceContextTextMapPropagator
 
+logging.basicConfig(level=os.environ.get('LOG_LEVEL', 'INFO'))
 logger = logging.getLogger(__name__)
 app = FastAPI()
+
+
+def with_trace(carrier: dict = None):
+
+    def decorator(func: Callable):
+
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            service_name = func.__name__
+            resource = Resource(attributes={SERVICE_NAME: service_name})
+            span_processor = BatchSpanProcessor(OTLPSpanGrpcExporter())
+            trace_provider = TracerProvider(
+                resource=resource, active_span_processor=span_processor)
+            trace.set_tracer_provider(trace_provider)
+            tracer = trace.get_tracer(service_name)
+            ctx = None
+            logger.debug(f"carrier: {carrier}")
+            if carrier:
+                ctx = TraceContextTextMapPropagator().extract(carrier=carrier)
+
+            logger.debug(f"ctx: {ctx}")
+            with tracer.start_as_current_span(service_name,
+                                              context=ctx) as span:
+                res = await func(*args, **kwargs)
+                return res
+
+        return wrapper
+
+    return decorator
 
 
 @app.get("/")
@@ -56,10 +101,10 @@ def iodoc(func):
         b = await r.body()
         iodoc_ev = IoDocumentEvent.model_validate_json(b)
         iodoc_obj = iodoc_ev.data
+        carrier = None
         if 'traceparent' in r.headers.keys():
-            [a, traceId, spanId, b] = r._headers.get('traceparent').split('-')
-            newSpanId = os.urandom(8).hex()
-            iodoc_obj.traceparent = f"{a}-{traceId}-{newSpanId}-{b}"
+            carrier = r.headers.get('traceparent')
+            iodoc_obj.traceparent = carrier
 
         if 'tracestate' in r.headers.keys():
             iodoc_obj.tracestate = r.headers.get('tracestate')
@@ -106,33 +151,42 @@ def iodoc(func):
 
             kwargs['graph'] = graphModel
 
-        value = await func(**kwargs)
-        logger.debug(f"return value: {value}")
+        @with_trace(carrier=r.headers)
+        @wraps(func)
+        async def inner(**kwargs):
+            value = await func(**kwargs)
 
-        if not value:
-            return Response(status_code=status.HTTP_200_OK)
-        else:
-            context_name = os.environ.get('CONTEXT_NAME', 'NO_CONTEXT')
+            logger.debug(f"return value: {value}")
 
-            obj_ev = CloudEvent.make_obj_event(value)
-            attributes = {
-                "id": uuid4().hex,
-                "source": f"mindwm.{context_name}.knfunc.{func.__name__}",
-                #"subject": f"{source}.feedback",
-                # TODO: fix the subject to variant from above when we implement new naming convention
-                "subject": f"mindwm.{username}.{hostname}.knfunc.feedback",
-                "type": obj_ev.type,
-                #"ce-traceparent": r.headers.get('ce-traceparent')
-            }
+            if not value:
+                return Response(status_code=status.HTTP_200_OK)
+            else:
+                context_name = os.environ.get('CONTEXT_NAME', 'NO_CONTEXT')
 
-            data = obj_ev.model_dump()
-            event = CE(attributes, data)
-            headers, body = to_structured(event)
-            #headers['content-type'] = 'application/cloudevents+json'
-            logger.debug(f"response: {headers}\n{body}")
-            response.headers.update(headers)
-            return JSONResponse(content=json.loads(body), headers=headers)
-        return value
+                obj_ev = CloudEvent.make_obj_event(value)
+                attributes = {
+                    "id": uuid4().hex,
+                    "source": f"mindwm.{context_name}.knfunc.{func.__name__}",
+                    #"subject": f"{source}.feedback",
+                    # TODO: fix the subject to variant from above when we implement new naming convention
+                    "subject": f"mindwm.{username}.{hostname}.knfunc.feedback",
+                    "type": obj_ev.type,
+                    #"ce-traceparent": r.headers.get('ce-traceparent')
+                }
+
+                data = obj_ev.model_dump()
+                event = CE(attributes, data)
+                headers, body = to_structured(event)
+                #headers['content-type'] = 'application/cloudevents+json'
+                logger.debug(f"response: {headers}\n{body}")
+                response.headers.update(headers)
+                return JSONResponse(content=json.loads(body), headers=headers)
+
+        res = await inner(**kwargs)
+        return res
+        #return inner
+
+    return wrapper
 
 
 def llm_answer(func):
@@ -151,33 +205,42 @@ def llm_answer(func):
             answer_ev = LLMAnswerEvent.model_validate_json(b)
             kwargs['answer'] = answer_ev.data
 
-        value = await func(**kwargs)
-        logger.debug(f"return value: {value}")
-        if 'traceparent' in r.headers.keys():
-            value.traceparent = r.headers.get('traceparent')
+        @with_trace(carrier=r.headers)
+        @wraps(func)
+        async def inner(**kwargs):
+            value = await func(**kwargs)
+            logger.debug(f"return value: {value}")
+            carrier = None
+            if 'traceparent' in r.headers.keys():
+                carrier = r.headers.get('traceparent')
+                iodoc_obj.traceparent = carrier
+                value.traceparent = carrier
 
-        if 'tracestate' in r.headers.keys():
-            value.tracestate = r.headers.get('tracestate')
-        logger.debug(f"with injected traces: {value}")
-        if not value:
-            return Response(status_code=status.HTTP_200_OK)
-        else:
-            context_name = os.environ.get('CONTEXT_NAME', 'NO_CONTEXT')
-            obj_ev = CloudEvent.make_obj_event(value)
-            attributes = {
-                "id": uuid4().hex,
-                "source": f"mindwm.{context_name}.knfunc.{func.__name__}",
-                #"subject": f"{source}.feedback",
-                # TODO: fix the subject to variant from above when we implement new naming convention
-                "subject": subject,
-                "type": obj_ev.type,
-                #"ce-traceparent": r.headers.get('ce-traceparent')
-            }
-            data = obj_ev.model_dump()
-            event = CE(attributes, data)
-            headers, body = to_structured(event)
-            #headers['content-type'] = 'application/cloudevents+json'
-            logger.debug(f"response: {headers}\n{body}")
-            response.headers.update(headers)
-            return JSONResponse(content=json.loads(body), headers=headers)
+            if 'tracestate' in r.headers.keys():
+                value.tracestate = r.headers.get('tracestate')
+            logger.debug(f"with injected traces: {value}")
+            if not value:
+                return Response(status_code=status.HTTP_200_OK)
+            else:
+                context_name = os.environ.get('CONTEXT_NAME', 'NO_CONTEXT')
+                obj_ev = CloudEvent.make_obj_event(value)
+                attributes = {
+                    "id": uuid4().hex,
+                    "source": f"mindwm.{context_name}.knfunc.{func.__name__}",
+                    #"subject": f"{source}.feedback",
+                    # TODO: fix the subject to variant from above when we implement new naming convention
+                    "subject": subject,
+                    "type": obj_ev.type,
+                    #"ce-traceparent": r.headers.get('ce-traceparent')
+                }
+                data = obj_ev.model_dump()
+                event = CE(attributes, data)
+                headers, body = to_structured(event)
+                #headers['content-type'] = 'application/cloudevents+json'
+                logger.debug(f"response: {headers}\n{body}")
+                response.headers.update(headers)
+                return JSONResponse(content=json.loads(body), headers=headers)
+            res = await inner(**kwargs)
+            return res
+
         return value
