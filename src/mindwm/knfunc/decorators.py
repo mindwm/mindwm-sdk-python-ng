f854e3287811112ad63a1f0a9fd4192d64bc8f83
Sergey Bubnov <omgbebebe@gmail.com>
2024-08-23 15:34:19 +0400

    try to use headers

--- src/mindwm/knfunc/decorators.py
+++ src/mindwm/knfunc/decorators.py
@@ -80,8 +80,9 @@ def event(func):
                 res_ev.subject = f"mindwm.{username}.{hostname}.knfnc.feedback"
                 logger.debug(f'reply with MindwmEvent: {res_ev}')
                 extra_headers['content-type'] = 'application/cloudevents+json'
-                resp = Response(content=res_ev.model_dump_json(),
-                                headers=extra_headers)
+                resp = to_response(res_ev, extra_headers)
+                #resp = Response(content=res_ev.model_dump_json(),
+                #                headers=extra_headers)
                 logger.debug(f'response body: {resp.body}')
                 logger.debug(f'response headers: {resp.headers}')
                 return resp
