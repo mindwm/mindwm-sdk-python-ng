65c77bdf619765cad145f1df52c90a2b2fcbd42f
Sergey Bubnov <omgbebebe@gmail.com>
2024-08-23 11:34:48 +0400

    refactor event model; helper to parse http request into MindwmEvent

--- src/mindwm/model/objects.py
+++ src/mindwm/model/objects.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Literal, Optional
 from uuid import uuid4
 
 from pydantic import BaseModel, Field
@@ -7,6 +7,7 @@ from pydantic import BaseModel, Field
 class MindwmObject(BaseModel):
     traceparent: Optional[str] = None
     tracestate: Optional[str] = None
+    type: str
 
     def to_json(self):
         return self.model_dump_json()
@@ -30,27 +31,41 @@ class IoDocument(MindwmObject):
     input: str
     output: str
     ps1: str
+    type: Literal['org.mindwm.v1.iodocument'] = 'org.mindwm.v1.iodocument'
 
 
 class Touch(MindwmObject):
     ids: List[int]
+    type: Literal['org.mindwm.v1.touch'] = 'org.mindwm.v1.touch'
 
 
 class LLMAnswer(MindwmObject):
     iodoc_uuid: str = Field(description='An original IoDocument uuid')
     codesnippet: str
     description: str
+    type: Literal['lorg.mindwm.v1.lmanswer'] = 'org.mindwm.v1.llmanswer'
 
 
 class Ping(MindwmObject):
     uuid: str = Field(description="uniq action id",
                       default_factory=lambda: uuid4().hex)
     payload: Optional[str] = None
+    type: Literal['org.mindwm.v1.ping'] = 'org.mindwm.v1.ping'
 
 
 class Pong(MindwmObject):
     payload: Optional[str] = None
     uuid: str
+    type: Literal['org.mindwm.v1.pong'] = 'org.mindwm.v1.pong'
+
+
+class KafkaCdc(MindwmObject):
+    knativearrivaltime: Optional[str] = None
+    key: Optional[str] = None
+    knativekafkaoffset: Optional[int] = None
+    knativekafkapartition: Optional[int] = None
+    partitionkey: Optional[str] = None
+    type: Literal['org.mindwm.v1.kafkacdc'] = 'org.mindwm.v1.kafkacdc'
 
 
 # Actions
@@ -62,14 +77,17 @@ class Action(MindwmObject):
         description=
         "non-empty list of targets to execute the action on. I.e. identifier of tmux pane"
     )
+    type: str
 
 
 class ShowMessage(Action):
     title: str
     message: str
+    type: Literal['org.mindwm.v1.showmessage'] = 'org.mindwm.v1.showmessage'
 
 
 class TypeText(Action):
     title: str
     description: str
     snippet: str
+    type: Literal['org.mindwm.v1.typetext'] = 'org.mindwm.v1.typetext'
