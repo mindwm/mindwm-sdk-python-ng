7f8a7d753747545370d9ec5c9bbb3216ca1f7cd7
Sergey Bubnov <omgbebebe@gmail.com>
2024-08-23 15:25:04 +0400

    fix subject

--- src/mindwm/knfunc/decorators.py
+++ src/mindwm/knfunc/decorators.py
@@ -77,7 +77,7 @@ def event(func):
                 #    res_ev.traceparent = extra_headers['traceparent']
 
                 res_ev.source = f"mindwm.{context_name}.knfunc.{func.__name__}"
-                res_ev.subject = f"mindwm.{username}.{hostname}.knfnc.feedback",
+                res_ev.subject = f"mindwm.{username}.{hostname}.knfnc.feedback"
                 logger.debug(f'reply with MindwmEvent: {res_ev}')
                 #resp = to_response(ev, extra_headers=extra_headers)
                 extra_headers['content-type'] = 'application/cloudevents+json'
