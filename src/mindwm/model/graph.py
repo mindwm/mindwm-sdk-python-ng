d6e057fadd70eff42f9981f88c9524dd02f46b16
Sergey Bubnov <omgbebebe@gmail.com>
2024-08-28 15:51:51 +0400

    our graph cdc events

--- src/mindwm/model/graph.py
+++ src/mindwm/model/graph.py
@@ -13,6 +13,7 @@ class MindwmNode(BaseNode):
     created: Optional[datetime] = None
     merged: Optional[datetime] = None
     traceparent: Optional[str] = None
+    type: str
     #    atime: datetime = Field(
     #    default_factory=datetime.now
     #)
@@ -22,12 +23,16 @@ class User(MindwmNode):
     __primarylabel__: ClassVar[str] = "User"
     __primaryproperty__: ClassVar[str] = "username"
     username: str
+    type: Literal[
+        'org.mindwm.v1.graph.node.user'] = 'org.mindwm.v1.graph.node.user'
 
 
 class Host(MindwmNode):
     __primarylabel__: ClassVar[str] = "Host"
     __primaryproperty__: ClassVar[str] = "hostname"
     hostname: str
+    type: Literal[
+        'org.mindwm.v1.graph.node.host'] = 'org.mindwm.v1.graph.node.host'
 
 
 class Tmux(MindwmNode):
@@ -35,24 +40,32 @@ class Tmux(MindwmNode):
     __primaryproperty__: ClassVar[str] = "socket_path"
     # TODO: define a proper validator
     socket_path: str  # f"{username}@{hostname}/{socket_path}"
+    type: Literal[
+        'org.mindwm.v1.graph.node.tmux'] = 'org.mindwm.v1.graph.node.tmux'
 
 
 class TmuxSession(MindwmNode):
     __primarylabel__: ClassVar[str] = "TmuxSession"
     __primaryproperty__: ClassVar[str] = "name"
     name: str  # f"{username}@{hostname}/{socket_path}:{tmux_session}"
+    type: Literal[
+        'org.mindwm.v1.graph.node.tmux_session'] = 'org.mindwm.v1.graph.node.tmux_session'
 
 
 class TmuxPane(MindwmNode):
     __primarylabel__: ClassVar[str] = "TmuxPane"
     __primaryproperty__: ClassVar[str] = "title"
     title: str  # f"{username}@{hostname}/{socket_path}:{tmux_session}%{pane}"
+    type: Literal[
+        'org.mindwm.v1.graph.node.tmux_pane'] = 'org.mindwm.v1.graph.node.tmux_pane'
 
 
 class IoDocument(MindwmNode, IoDocument):
     __primarylabel__: ClassVar[str] = "IoDocument"
     __primaryproperty__: ClassVar[str] = "uuid"
     uuid: str
+    type: Literal[
+        'org.mindwm.v1.graph.node.iodocument'] = 'org.mindwm.v1.graph.node.iodocument'
 
 
 class Clipboard(MindwmNode):
@@ -61,6 +74,8 @@ class Clipboard(MindwmNode):
     uuid: str
     time: int
     data: str
+    type: Literal[
+        'org.mindwm.v1.graph.node.clipboard'] = 'org.mindwm.v1.graph.node.clipboard'
 
 
 class Parameter(MindwmNode):
@@ -68,6 +83,8 @@ class Parameter(MindwmNode):
     __primaryproperty__: ClassVar[str] = "key"
     key: str
     val: str
+    type: Literal[
+        'org.mindwm.v1.graph.node.parameter'] = 'org.mindwm.v1.graph.node.parameter'
 
 
 # Relations
@@ -75,84 +92,147 @@ class MindwmRelationship(BaseRelationship):
     traceparent: Optional[str] = None
     created: Optional[datetime] = None
     merged: Optional[datetime] = None
+    type: str
 
 
 class UserHasHost(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_HOST"
     source: User
     target: Host
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.user_has_host'] = 'org.mindwm.v1.graph.relationship.user_has_host'
 
 
 class HostHasTmux(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_TMUX"
     source: Host
     target: Tmux
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.host_has_tmux'] = 'org.mindwm.v1.graph.relationship.host_has_tmux'
 
 
 class TmuxHasTmuxSession(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_TMUX_SESSION"
     source: Tmux
     target: TmuxSession
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.tmux_has_session'] = 'org.mindwm.v1.graph.relationship.tmux_has_session'
 
 
 class TmuxSessionHasTmuxPane(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_TMUX_PANE"
     source: TmuxSession
     target: TmuxPane
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.tmux_session_has_pane'] = 'org.mindwm.v1.graph.relationship.tmux_session_has_pane'
 
 
 class TmuxPaneHasIoDocument(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_IO_DOCUMENT"
     source: TmuxPane
     target: IoDocument
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.tmux_pane_has_iodocument'] = 'org.mindwm.v1.graph.relationship.tmux_pane_has_iodocument'
 
 
 class HostHasClipboard(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_CLIPBOARD"
     source: Host
     target: Clipboard
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.host_has_clipboard'] = 'org.mindwm.v1.graph.relationship.host_has_clipboard'
 
 
 class UserHasTmux(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_TMUX"
     source: User
     target: Tmux
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.user_has_tmux'] = 'org.mindwm.v1.graph.relationship.user_has_tmux'
 
 
 class IoDocumentHasUser(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_USER"
     source: IoDocument
     target: User
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.iodocument_has_user'] = 'org.mindwm.v1.graph.relationship.iodocument_has_user'
 
 
 class UserHasParameter(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_PARAMETER"
     source: User
     target: Parameter
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.user_has_parameter'] = 'org.mindwm.v1.graph.relationship.user_has_parameter'
 
 
 class HostHasParameter(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_PARAMETER"
     source: Host
     target: Parameter
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.host_has_parameter'] = 'org.mindwm.v1.graph.relationship.host_has_parameter'
 
 
 class TmuxHasParameter(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_PARAMETER"
     source: Tmux
     target: Parameter
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.tmux_has_parameter'] = 'org.mindwm.v1.graph.relationship.tmux_has_parameter'
 
 
 class TmuxSessionHasParameter(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_PARAMETER"
     source: TmuxSession
     target: Parameter
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.tmux_session_has_parameter'] = 'org.mindwm.v1.graph.relationship.tmux_session_has_parameter'
 
 
 class TmuxPaneHasParameter(MindwmRelationship):
     __relationshiptype__: ClassVar[str] = "HAS_PARAMETER"
     source: TmuxPane
     target: Parameter
+    type: Literal[
+        'org.mindwm.v1.graph.relationship.tmux_pane_has_parameter'] = 'org.mindwm.v1.graph.relationship.tmux_pane_has_parameter'
+
+
+# cloudvent
+# source: `org.midwm.context.cyan.knfunc.kafka_cdc
+# subject: org.mindwm.context.cyan.graph.node
+# type: 'org.mibndwm.v1.graph_change' # payload type
+
+Prop = TypeVar("Prop", User, Host, Tmux, TmuxSession, TmuxPane, IoDocument,
+               Clipboard, Parameter)
+
+ChangedObject = Annotated[Union[User, Host, Tmux, TmuxSession, TmuxPane,
+                                IoDocument, Clipboard, Parameter, UserHasHost,
+                                HostHasTmux, HostHasClipboard,
+                                HostHasParameter, TmuxHasTmuxSession,
+                                TmuxHasParameter, TmuxSessionHasTmuxPane,
+                                TmuxSessionHasParameter, TmuxPaneHasIoDocument,
+                                TmuxPaneHasParameter],
+                          Field(discriminator='type')]
+
+
+class GraphObjectCreated(BaseModel):
+    type: Literal[
+        'org.mindwm.v1.graph.created'] = 'org.mindwm.v1.graph.created'
+    properties: ChangedObject
+
+
+class GraphObjectUpdated(BaseModel):
+    type: Literal[
+        'org.mindwm.v1.graph.updated'] = 'org.mindwm.v1.graph.updated'
+    properties: ChangedObject
+
+
+class GraphObjectDeleted(BaseModel):
+    type: Literal[
+        'org.mindwm.v1.graph.deleted'] = 'org.mindwm.v1.graph.deleted'
+    properties: ChangedObject
 
 
 # kafka-source cdc events
@@ -164,10 +244,6 @@ class KafkaCdcMeta(BaseModel):
     txEventsCount: int
     operation: str
     source: Dict[str, str]
-
-
-Prop = TypeVar("Prop", User, Host, Tmux, TmuxSession, TmuxPane, IoDocument,
-               Clipboard, Parameter)
 
 
 class KafkaCdcRelNode(BaseModel):
@@ -225,7 +301,9 @@ class KafkaCdc(BaseModel):
         return self.payload.after.properties
 
     def get_object(self):
-        if self.payload.meta.operation != 'deleted':
+        if type(self.payload) is KafkaCdcRelation:
+            return self.payload
+        if self.meta.operation != 'deleted':
             return self.get_object_after()
         else:
             return self.get_object_before()
--- tests/knfunc/kafka_cdc.py
+++ tests/knfunc/kafka_cdc.py
@@ -1,7 +1,8 @@
 from mindwm import logging
 from mindwm.knfunc.decorators import Request, Response, app, event
-from mindwm.model.events import from_request, to_response
-from mindwm.model.graph import KafkaCdc
+from mindwm.model.events import MindwmEvent, from_request, to_response
+from mindwm.model.graph import (GraphObjectCreated, GraphObjectDeleted,
+                                GraphObjectUpdated, KafkaCdc)
 
 logger = logging.getLogger(__name__)
 
@@ -9,15 +10,21 @@ logger = logging.getLogger(__name__)
 @event
 async def func(obj: KafkaCdc, request: Request, response: Response):
     logger.info(obj)
-    ev = await from_request(request)
-    match obj.payload.type:
-        case 'relationship':
-            ev.traceparent = obj.payload.traceparent
-        case 'node':
-            if obj.meta.operation != 'deleted':
-                ev.traceparent = obj.payload.after.properties.traceparent
-            else:
-                ev.traceparent = obj.payload.before.properties.traceparent
+    cdc_ev = await from_request(request)
+    graph_object = obj.get_object()
+    match obj.meta.operation:
+        case 'created':
+            ev_payload = GraphObjectCreated(properties=graph_object)
+        case 'updated':
+            ev_payload = GraphObjectUpdated(properties=graph_object)
+        case 'deleted':
+            GraphObjectDeleted(properties=graph_object)
 
-    resp = to_response(ev)
+    new_ev = MindwmEvent(
+        source=f"org.mindwm.context.cyan.knfunc.kafka_cdc",
+        subject=f"org.mindwm.context.cyan.graph.{cdc_ev.data.payload.type}",
+        type=ev_payload.type,
+        data=ev_payload,
+        traceparent=graph_object.traceparent)
+    resp = to_response(new_ev)
     return resp
